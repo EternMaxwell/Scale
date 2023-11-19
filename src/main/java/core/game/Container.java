f9f5b2417069cfbfe2633984a697176a6a6f45a9
Etern Maxwell <maxwell_dev@outlook.com>
2023-11-19 23:08:17 +0800

    game core container there are some faults in the transformation matrix initialization, but it works like what I want. I don't understand but it works. Maybe because I used left-handed coordinate

--- src/main/java/core/game/Container.java
+++ src/main/java/core/game/Container.java
@@ -1,8 +1,6 @@
 package core.game;
 
-import org.joml.Matrix4f;
-import org.joml.Vector2f;
-import org.joml.Vector3f;
+import org.joml.*;
 
 public class Container {
     private final Vector3f vertices[] = new Vector3f[3];
@@ -22,17 +20,40 @@ public class Container {
         vertices[0] = vertex1;
         vertices[1] = vertex2;
         vertices[2] = vertex3;
-        normal = new Vector3f();
-        normal = vertex2.sub(vertex1, new Vector3f()).cross(vertex3.sub(vertex1, new Vector3f()));
+        normal = vertex2.sub(vertex1, new Vector3f()).cross(vertex3.sub(vertex1, new Vector3f())).negate();
         normal.normalize();
-        xDir = new Vector3f();
         xDir = vertex2.sub(vertex1, new Vector3f());
         xDir.normalize();
-        yDir = new Vector3f();
-        yDir = normal.cross(xDir, new Vector3f());
+        yDir = new Vector3f(normal).cross(xDir, new Vector3f());
         yDir.normalize();
         transform3to2 = new Matrix4f();
-        transform3to2.identity().setLookAt(new Vector3f(vertex1).add(normal), vertex1, yDir);
+        transform3to2.translate(vertex1).lookAlong(normal, yDir);
+        transform3to2.setLookAtLH(vertex1, vertex1.add(normal, new Vector3f()), yDir);
+        transform2to3 = new Matrix4f(transform3to2).invert();
+    }
+
+    /**
+     * set the vertices of the container.
+     * @param vertex1 The first vertex.
+     * @param vertex2 The second vertex.
+     * @param vertex3 The third vertex.
+     * @return the container.
+     */
+    public Container setVertices(Vector3f vertex1, Vector3f vertex2, Vector3f vertex3) {
+        vertices[0] = vertex1;
+        vertices[1] = vertex2;
+        vertices[2] = vertex3;
+        normal = vertex2.sub(vertex1, new Vector3f()).cross(vertex3.sub(vertex1, new Vector3f())).negate();
+        normal.normalize();
+        xDir = vertex2.sub(vertex1, new Vector3f());
+        xDir.normalize();
+        yDir = new Vector3f(normal).cross(xDir, new Vector3f());
+        yDir.normalize();
+        transform3to2 = new Matrix4f();
+        transform3to2.translate(vertex1).lookAlong(normal, yDir);
+        transform3to2.setLookAtLH(vertex1, vertex1.add(normal, new Vector3f()), yDir);
+        transform2to3 = new Matrix4f(transform3to2).invert();
+        return this;
     }
 
     /**
@@ -76,12 +97,32 @@ public class Container {
 
     /**
      * get the absolute position of the given position in the container.
+     * <p>Use the direct method.</p>
      * @param posInContainer The position in the container.
      * @return the absolute position.
      */
-    public Vector3f absPosition(Vector2f posInContainer){
-        Vector3f pos = new Vector3f();
-        pos = vertices[0].add(xDir.mul(posInContainer.x, new Vector3f())).add(yDir.mul(posInContainer.y, new Vector3f()));
+    public Vector3f absPositionDirect(Vector2f posInContainer){
+        Vector3f pos = new Vector3f(vertices[0]).add(xDir.mul(posInContainer.x, new Vector3f())).add(yDir.mul(posInContainer.y, new Vector3f()));
         return pos;
+    }
+
+    /**
+     * get the absolute position of the given position in the container.
+     * <p>Use the transform method.</p>
+     * @param posInContainer The position in the container.
+     * @return the absolute position.
+     */
+    public Vector3f absPositionTransform(Vector2f posInContainer){
+        Vector3f temp = new Vector3f(posInContainer, 0);
+        return transform2to3.transformProject(temp);
+    }
+
+    public static void main(String[] args) {
+        Container container = new Container(new Vector3f(1, 0, 0), new Vector3f(2, 4, 0), new Vector3f(0, 1, 0));
+        System.out.println(new Vector2f(container.transform3to2.transformProject(new Vector3f(2,4,0)).x,
+                                        container.transform3to2.transformProject(new Vector3f(2,4,0)).y));
+        Vector2f posInContainer = new Vector2f(4.123f,0);
+        System.out.println(container.absPositionDirect(posInContainer));
+        System.out.println(container.absPositionTransform(posInContainer));
     }
 }
