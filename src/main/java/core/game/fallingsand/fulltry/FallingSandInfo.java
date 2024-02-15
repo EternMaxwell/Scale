6efb34359e3989ca0eec1bf885ead8caf7dbc975
Etern Maxwell <maxwell_dev@outlook.com>
2024-02-15 21:08:03 +0800

    target: updating new-feature: preparing for further implementation for falling sand world

---
+++ src/main/java/core/game/fallingsand/fulltry/FallingSandInfo.java
@@ -0,0 +1,8 @@
+package core.game.fallingsand.fulltry;
+
+public class FallingSandInfo {
+    public static float scale = 1;
+    public static int defaultShowGridWidth = 512;
+    public static double[] cameraCentrePos = new double[]{0, 0};
+
+}
